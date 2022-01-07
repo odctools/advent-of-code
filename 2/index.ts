75d02005f73b252a86d4e3be3df58a86b99dd698
Timothy Kou <timothydkou@gmail.com>
2022-01-06 21:19:45 -1000

    part 2 done

--- 2/index.ts
+++ 2/index.ts
@@ -1,3 +1,4 @@
+import { Console } from "console";
 import { readFileSync, createReadStream } from "fs";
 import * as readline from "readline";
 
@@ -34,7 +35,46 @@ async function part1() {
   return (horizontal * depth);
 }
 
+async function part2() {
+  let horizontal: number = 0;
+  let depth: number = 0;
+  let aim: number = 0;
+
+  const fileStream = createReadStream("./2/input.txt");
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    const split = line.split(" ");
+    const direction = split[0];
+    const units = +split[1];
+
+    switch (direction) {
+      case 'down': 
+        aim += units;
+        break;
+      case 'up':
+        aim -= units;
+        break;
+      case 'forward':
+        horizontal += units;
+        depth += units * aim;
+        break;
+      default:
+        console.log('no direction found')
+    }
+
+    // can't go higher than the surface! I'm guessing...
+    if (depth < 0) depth = 0;
+  }
+
+  return (horizontal * depth);
+}
+
 (async function () {
   console.log(await part1());
-  // console.log(await part2());
+  console.log(await part2());
 })();
