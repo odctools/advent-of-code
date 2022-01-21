e36a7867d20813564f8891d4889e48b4f1f114c5
Timothy Kou <timothydkou@gmail.com>
2022-01-20 21:38:04 -0500

    part 1 done

--- 4/index.ts
+++ 4/index.ts
@@ -1,5 +1,23 @@
 import { readFileSync, createReadStream, read } from "fs";
 import * as readline from "readline";
+const _ = require("lodash");
+
+const bingoWins = [
+    // ROWS
+    [0,1,2,3,4],
+    [5,6,7,8,9],
+    [10,11,12,13,14],
+    [15,16,17,18,19],
+    [20,21,22,23,24],
+
+    // COLS
+    [0,5,10,15,20],
+    [1,6,11,16,21],
+    [2,7,12,17,22],
+    [3,8,13,18,23],
+    [4,9,14,19,24],
+]
+
 
 const readlines = async (fp: string) => {
 
@@ -12,25 +30,120 @@ const readlines = async (fp: string) => {
     }
 
     const drawings = data[0].split(',').map(s => parseInt(s));
-    const boards = data.splice(1).map(row => {
+    const boardRows = data.splice(1).map(row => {
         return row.trim().split(' ')
             .filter(v => v != '')
             .map(v => parseInt(v))
-            
     });
+
+    const boards: BoardClass[] = [];
+    let board: number[] = [];
+    let rowCount = 0;
+    for (const row of boardRows) {
+        if (row && row.length > 0) {
+            board.push(...row)
+            rowCount++;
+        };
+
+        if (rowCount === 5) {
+            boards.push(new BoardClass(board));
+            board = [];
+            rowCount = 0;
+        }
+    }
 
     return {drawings, boards};
 };
 
+class BoardClass
+{
+    board: number[];
+    // holds in the indices of the numbers that have been drawn on this board
+    drawn: number[];
+
+    constructor(board: number[]) {
+        this.board = board;
+        this.drawn = [];
+    }
+}
+
+const draw = async (boards: BoardClass[], num: number) => {
+    // iterate through each board and check if the number is in the board
+    for (const boardClass of boards) {
+        if (boardClass.board.includes(num)) {
+            boardClass.drawn.push(boardClass.board.findIndex(v => v === num));
+            boardClass.drawn = _.sortBy(boardClass.drawn)
+        }
+    }
+    
+    return;
+}
+
+const bingo = async (boards: BoardClass[]): Promise<{winningBoards: number[], winningNums: number[]}> => {
+    const winningBoards: number[] = [];
+    let winningNums: number[] = [];
+    // iterate through each board and check if a board has bingo
+    boards.forEach((boardClass, i) => {
+        if (boardClass.drawn.length < 5) return
+        
+        let win = 0;
+        for (const bingoCombo of bingoWins) {
+            winningNums = [];
+
+            for (const num of bingoCombo) {
+                if (!boardClass.drawn.includes(num)) {
+                    break
+                } else winningNums.push(num)
+            }
+             
+            if (winningNums.length === 5) {
+                // reached end of bingocombo
+                winningBoards.push(i);
+                break;
+            }
+            
+        }
+    })
+    return {winningBoards, winningNums};
+}
+
+const scoreWinner = async (winningBoard: BoardClass, winningIxs: number[], lastNumCalled: number): Promise<number> => {
+    // given indexes of drawn numbers, get the actual winning numbers
+    const winningNums: number[] = winningBoard.drawn.map(v => winningBoard.board[v]);
+
+    // get all unmarked numbers
+    const remainingNums: number[] = winningBoard.board.filter(v => !winningNums.includes(v));
+    const sumRemainingNums = _.sum(remainingNums)
+    
+    return sumRemainingNums * lastNumCalled;
+}
+
 
 const part1 = async () => {
     const input = await readlines("./4/input.txt");
-
     const drawings = input?.drawings;
     const boards = input?.boards;
 
-    console.log(drawings, boards)
+    if (!drawings || !boards) {
+        console.log(`ERROR with reading lines.`)
+        return
+    }
 
+    for (const num of drawings) {
+        await draw(boards, num);
+
+        const {winningBoards, winningNums} = await bingo(boards);
+        if (winningBoards && winningBoards.length > 0) {
+
+            if (winningBoards.length > 1) {
+                console.log('ERROR: MORE THAN ONE BOARD WON:', winningBoards)
+            } else {
+                return await scoreWinner(boards[winningBoards[0]], winningNums, num)
+            }
+
+            return
+        }
+    }
 }
 
 (async function () {
